6281874da93cea30c66de3e82634e873123ad732
Pavel-Troinich <tr.pav.ar@gmail.com>
2022-10-08 23:18:10 +0300

    feat: add testimonials-slider

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -24,3 +24,27 @@ function closeBurger() {
   switchMenu();
   shadow.removeEventListener('click', closeBurger);
 }
+
+
+// Testimonials slider
+
+const slider = document.querySelector('.testimonials__slider');
+const testimonials = document.querySelector('.testimonials-content');
+slider.value = 0;
+slider.addEventListener('input', () => {
+  if (document.documentElement.clientWidth > 1220) {
+    testimonials.style.transform = 'translateX(-' + 297 * slider.value + 'px)';
+  } else {
+    testimonials.style.transform = 'translateX(-' + 323 * slider.value + 'px)';
+  }  
+});
+
+window.addEventListener('resize', () => {
+  if (document.documentElement.clientWidth < 1000) {
+    slider.value = 0;
+    testimonials.style.transform = 'translateX(0px)';
+  }  
+});
+
+
+
